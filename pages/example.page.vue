6db1071459c313a401dd2a283140779055adf7b8
Justin Wash <justin.wash@pm.me>
2019-05-06 15:29:17 -0500

    add github banner

--- pages/example.page.vue
+++ pages/example.page.vue
@@ -1,31 +1,44 @@
 <template>
 	<div class="example-page">
+		<a href="https://github.com/justinwash/client-side-vue" target="_blank"
+			><img
+				width="149"
+				height="149"
+				src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149"
+				class="github-banner"
+				alt="Fork me on GitHub"
+				data-recalc-dims="1"
+		/></a>
 		<div class="section">
-			<h3>Binding</h3>Click button to increment counter. Standard Vue stuff here.
-			<br>
-			<br>
+			<h3>Binding</h3>
+			Click button to increment counter. Standard Vue stuff here.
+			<br />
+			<br />
 			<code>this.counter: {{ counter }}</code>
 			<button v-on:click="increment()">Click Me!</button>
 		</div>
 		<div class="section">
-			<h3>State</h3>This page is reading a store (services/store.service.js) key "myObject" which has the value:
-			<br>
-			<br>
+			<h3>State</h3>
+			This page is reading a store (services/store.service.js) key "myObject"
+			which has the value:
+			<br />
+			<br />
 			<code>{{ storeExample }}</code>
-			<br>
-			<br>If you don't see anything above you either refreshed or haven't navigated to the home page (which, for the purposes of this example, sets the store value).
-			<br>This value is only persistent while navigating using VueRouter or the back/forward browser buttons. If you
-			<a
-				href="/#/example"
-				v-on:click="refresh()"
-			>refresh</a> this page it will disappear until navigating to
+			<br />
+			<br />If you don't see anything above you either refreshed or haven't
+			navigated to the home page (which, for the purposes of this example, sets
+			the store value). <br />This value is only persistent while navigating
+			using VueRouter or the back/forward browser buttons. If you
+			<a href="/#/example" v-on:click="refresh()">refresh</a> this page it will
+			disappear until navigating to
 			<router-link to="/">the home page</router-link>&nbsp;and back again.
 		</div>
 		<div class="section">
-			<h3>Http</h3>This page is using axios.js (dependencies/axios.js) to make an http request to
-			<code>https://httpbin.org/get</code>. It's response was:
-			<br>
-			<br>
+			<h3>Http</h3>
+			This page is using axios.js (dependencies/axios.js) to make an http
+			request to <code>https://httpbin.org/get</code>. It's response was:
+			<br />
+			<br />
 			<div id="http-response">
 				<code>{{ httpResponse }}</code>
 			</div>
@@ -35,16 +48,16 @@
 
 <script>
 	module.exports = {
-		data: function () {
+		data: function() {
 			return {
 				storeExample: null,
 				httpResponse: null,
 				counter: 0
-			}
+			};
 		},
 		methods: {
 			getStateExample() {
-				this.storeExample = App._store.get('myObject');
+				this.storeExample = App._store.get("myObject");
 			},
 			increment() {
 				this.counter = this.counter + 1;
@@ -56,11 +69,11 @@
 		mounted() {
 			this.getStateExample();
 
-			axios.get('https://httpbin.org/get').then(res => {
+			axios.get("https://httpbin.org/get").then(res => {
 				this.httpResponse = res.data;
-			})
+			});
 		}
-	}
+	};
 </script>
 
 <style scoped>
@@ -78,5 +91,11 @@
 	p {
 		font-size: 2em;
 		text-align: center;
+	}
+
+	.github-banner {
+		position: fixed;
+		top: 0;
+		right: 0;
 	}
 </style>
--- pages/home.page.vue
+++ pages/home.page.vue
@@ -1,93 +1,99 @@
 <template>
 	<div class="home-page">
 		<main>
+			<a href="https://github.com/justinwash/client-side-vue" target="_blank"
+				><img
+					width="149"
+					height="149"
+					src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149"
+					class="github-banner"
+					alt="Fork me on GitHub"
+					data-recalc-dims="1"
+			/></a>
 			<p>{{ greeting }}</p>
 			<div class="section">
-				<h3>About</h3>Vue.js client-side for tiny, quick-loading, node.js-less Single Page Apps.
+				<h3>About</h3>
+				Vue.js client-side for tiny, quick-loading, node.js-less Single Page
+				Apps.
 			</div>
 			<div class="section">
 				<h3>Breakdown</h3>
 
 				<h4>client-side-vue/</h4>
 				<li>
-					<b>index.html</b> - The entry-point for your Single Page App.
-					Global dependencies via script tags (Vue, Vue-Router, Http-Vue-Loader, Axios) and a single #App element where the vue instance will be rendered.
+					<b>index.html</b> - The entry-point for your Single Page App. Global
+					dependencies via script tags (Vue, Vue-Router, Http-Vue-Loader, Axios)
+					and a single #App element where the vue instance will be rendered.
 				</li>
 				<li>
-					<b>index.js</b> - Where Vue.js, Vue-Router are instantiated, where you'll import and assign your services to the Vue instance.
+					<b>index.js</b> - Where Vue.js, Vue-Router are instantiated, where
+					you'll import and assign your services to the Vue instance.
 				</li>
-				<li>
-					<b>index.css</b> - Global styles for your Single Page App.
-				</li>
-				<li>
-					<b>index.php</b> - Trick Heroku into hosting our static site.
-				</li>
+				<li><b>index.css</b> - Global styles for your Single Page App.</li>
+				<li><b>index.php</b> - Trick Heroku into hosting our static site.</li>
 
 				<h4>client-side-vue/dependencies/</h4>
 				<li>
-					<b>vue.js</b> - Vue.js itself and all the built-in features that come with it.
-					<a
-						href="https://github.com/vuejs/vue"
-						target="_blank"
-					>GitHub</a>
+					<b>vue.js</b> - Vue.js itself and all the built-in features that come
+					with it.
+					<a href="https://github.com/vuejs/vue" target="_blank">GitHub</a>
 				</li>
 				<li>
-					<b>vue-router.js</b> - The official router for Vue.js, allows you to navigate your Single Page App.
-					<a
-						href="https://github.com/vuejs/vue-router"
-						target="_blank"
-					>GitHub</a>
+					<b>vue-router.js</b> - The official router for Vue.js, allows you to
+					navigate your Single Page App.
+					<a href="https://github.com/vuejs/vue-router" target="_blank"
+						>GitHub</a
+					>
 				</li>
 				<li>
 					<b>http-vue-loader.js</b> - Client-side .vue component loader.
 					<a
 						href="https://github.com/FranckFreiburger/http-vue-loader"
 						target="_blank"
-					>GitHub</a>
+						>GitHub</a
+					>
 				</li>
 				<li>
 					<b>axios.js</b> - Promise based HTTP client for making ajax/http calls
-					<a
-						href="https://github.com/axios/axios"
-						target="_blank"
-					>GitHub</a>
+					<a href="https://github.com/axios/axios" target="_blank">GitHub</a>
 				</li>
-				<br>Minified versions of these dependencies (
-				<code>*.min.js</code>) are included for speed/size.
+				<br />Minified versions of these dependencies ( <code>*.min.js</code>)
+				are included for speed/size.
 				<h4>client-side-vue/services</h4>
 				<li>
-					<b>store.service.js</b> - A (very, extremely) simple state-management service with only three methods,
-					<code>get()</code>,
-					<code>set()</code>, and
-					<code>clear()</code>. This page set a store key "myObject" to an instantiation of the global class MyClass.
-					Click over to the
-					<router-link to="/example">example page</router-link>&nbsp;to retrieve it and see more features.
+					<b>store.service.js</b> - A (very, extremely) simple state-management
+					service with only three methods, <code>get()</code>,
+					<code>set()</code>, and <code>clear()</code>. This page set a store
+					key "myObject" to an instantiation of the global class MyClass. Click
+					over to the
+					<router-link to="/example">example page</router-link>&nbsp;to retrieve
+					it and see more features.
 				</li>
 			</div>
 		</main>
 		<aside>
-			<img class="directory" src="assets/directory.png">
+			<img class="directory" src="assets/directory.png" />
 		</aside>
 	</div>
 </template>
 
 <script>
 	module.exports = {
-		data: function () {
+		data: function() {
 			return {
-				greeting: 'Client-side Vue.js',
+				greeting: "Client-side Vue.js",
 				httpResponse: null
-			}
+			};
 		},
 		methods: {
 			setStateExample() {
-				App._store.set('myObject', new MyClass());
+				App._store.set("myObject", new MyClass());
 			}
 		},
 		mounted() {
 			this.setStateExample();
 		}
-	}
+	};
 </script>
 
 <style scoped>
@@ -120,5 +126,11 @@
 		.home-page {
 			flex-direction: column !important;
 		}
+	}
+
+	.github-banner {
+		position: fixed;
+		top: 0;
+		right: 0;
 	}
 </style>
